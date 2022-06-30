fbff768de7a5333092c566adf6d6d153d4f8be42
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-06-30 23:59:11 +0300

    Fourth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -67,119 +67,115 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
 
-    return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 75.0, bottom: 20),
-            child: Container(
-              height: 200,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(width: 3, color: Colors.blue.shade100),
-                image: DecorationImage(
-                    image: Image.network(
-                            "http://4.bp.blogspot.com/-VlHRlPoXwqU/TtskdJMC4aI/AAAAAAAAAjQ/ooLDREJgZvM/s1600/parrot1.jpg")
-                        .image,
-                    fit: BoxFit.fill),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20, bottom: 20),
+              child: Container(
+                height: 220,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 5, color: Colors.blue.shade100),
+                  image: DecorationImage(
+                      image: Image.network(
+                              "https://scontent.febl5-2.fna.fbcdn.net/v/t1.6435-9/204742823_985874458852420_4469077668754906067_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHcJIuCl4rRONwFOZJoHlv8EU-Nnz8GSfcRT42fPwZJ9-t3OXtMdDLW7HdnzlalofPU5NM0AyuZaUaRcgf3A0hP&_nc_ohc=kRNUR29EUskAX8wNKqK&_nc_ht=scontent.febl5-2.fna&oh=00_AT9GhP06Krsq9KBPuHdHcb9ArsNkxLoWgPluIZppZ-F7CA&oe=62E26370")
+                          .image,
+                      ),
+                ),
               ),
             ),
-          ),
-          const Text(
-            "Full Name",
-            style: TextStyle(fontSize: 25, letterSpacing: 1.5),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          const Center(
-            child: Text(
-              "Lorem ipsum dolor sit amet,\nconsectetur adipiscingkjdfh elkjhdfit,\n sed do eiusmod tlksaflkalempor .",
-              style: TextStyle(fontSize: 16, wordSpacing: 2),
-              textAlign: TextAlign.center,
+            const Text(
+              "Ahmad Shakir Khalid",
+              style: TextStyle(fontSize: 24, letterSpacing: 1.5),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          containerButton(
-              urlText: "LinkedIn",
-              icon: FontAwesomeIcons.linkedin,
-              color: const Color(0xFF3CACAE),
-              onPressed: () {
-                const url =
-                    "https://www.linkedin.com/in/ahmad-shakir-1a6a95226/";
-                _launch(url);
-              }),
-          const SizedBox(
-            height: 20,
-          ),
-          containerButton(
-              urlText: "Facebook",
-              icon: FontAwesomeIcons.facebook,
-              color: const Color(0xFFE0A030),
-              onPressed: () {
-                const url = "https://www.facebook.com/Ahmad.Shakir.Khalid";
-                _launch(url);
-              }),
-          const SizedBox(
-            height: 20,
-          ),
-          containerButton(
-              urlText: "Twitter",
-              icon: FontAwesomeIcons.twitter,
-              color: const Color(0xFFFF2768),
-              onPressed: () {
-                const url = "https://twitter.com/home";
-                _launch(url);
-              }),
-          const SizedBox(
-            height: 20,
-          ),
-          containerButton(
-              urlText: "Github",
-              icon: FontAwesomeIcons.github,
-              color: const Color(0xFFC26DBC),
-              onPressed: () {
-                const url = "https://github.com/ahmadshakir21";
-                _launch(url);
-              }),
-          const SizedBox(
-            height: 20,
-          ),
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            const CircleAvatar(
-                radius: 30,
-                backgroundColor: Colors.greenAccent,
-                child: FaIcon(
-                  FontAwesomeIcons.whatsapp,
-                  size: 30,
-                )),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Message(),
-                    ));
-              },
-              child: const CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Color.fromARGB(255, 240, 193, 105),
-                  child: FaIcon(
-                    FontAwesomeIcons.solidMessage,
-                    size: 30,
-                  )),
+            const SizedBox(
+              height: 20,
             ),
-            const CircleAvatar(
-                radius: 30,
-                backgroundColor: Color.fromARGB(255, 213, 240, 105),
-                child: FaIcon(
-                  FontAwesomeIcons.anchor,
-                  size: 30,
-                )),
-          ]),
-        ],
+            const Center(
+              child: Text(
+                "Lorem ipsum dolor sit amet,\nconsectetur adipiscingkjdfh elkjhdfit,\n sed do eiusmod tlksaflkalempor .",
+                style: TextStyle(fontSize: 15, wordSpacing: 2),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            containerButton(
+                urlText: "LinkedIn",
+                icon: FontAwesomeIcons.linkedin,
+                color: const Color(0xFF3CACAE),
+                onPressed: () {
+                  const url =
+                      "https://www.linkedin.com/in/ahmad-shakir-1a6a95226/";
+                  _launch(url);
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            containerButton(
+                urlText: "Facebook",
+                icon: FontAwesomeIcons.facebook,
+                color: const Color(0xFFE0A030),
+                onPressed: () {
+                  const url = "https://www.facebook.com/Ahmad.Shakir.Khalid";
+                  _launch(url);
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            containerButton(
+                urlText: "Twitter",
+                icon: FontAwesomeIcons.twitter,
+                color: const Color(0xFFFF2768),
+                onPressed: () {
+                  const url = "https://twitter.com/home";
+                  _launch(url);
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            containerButton(
+                urlText: "Github",
+                icon: FontAwesomeIcons.github,
+                color: const Color(0xFFC26DBC),
+                onPressed: () {
+                  const url = "https://github.com/ahmadshakir21";
+                  _launch(url);
+                }),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              const FaIcon(
+                FontAwesomeIcons.whatsapp,
+                size: 40,
+                color: Color(0xFF25D366),
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Message(),
+                      ));
+                },
+                child: const Icon(
+                  Icons.chat,
+                  size: 40,
+                  color: Color(0xFF333652),
+                ),
+              ),
+              const FaIcon(
+                FontAwesomeIcons.telegram,
+                size: 40,
+                color: Color(0xFF0088CC),
+              ),
+            ]),
+          ],
+        ),
       ),
     );
   }
