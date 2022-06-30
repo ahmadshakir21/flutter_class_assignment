9f84db43f625616d50c00721a320f4b0a0c72de0
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-06-30 17:52:42 +0300

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/message.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,36 +14,51 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+_launchURLBrowserLinkedin() async {
+  var url = Uri.parse("https://www.linkedin.com/in/ahmad-shakir-1a6a95226/");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchURLBrowserFacebook() async {
+  var url = Uri.parse("https://www.facebook.com/Ahmad.Shakir.Khalid");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchURLBrowserTwitter() async {
+  var url = Uri.parse("https://twitter.com/home");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchURLBrowserGitHub() async {
+  var url = Uri.parse("https://github.com/ahmadshakir21");
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +67,143 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String linkedinUrl = "https://www.linkedin.com/in/ahmad-shakir-1a6a95226/";
+  String facebookUrl = "https://www.facebook.com/Ahmad.Shakir.Khalid";
+  String twitterUrl = "https://twitter.com/home";
+  String githubUrl = "https://github.com/ahmadshakir21";
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 75.0, bottom: 20),
+            child: Container(
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(width: 3, color: Colors.blue.shade100),
+                image: DecorationImage(
+                    image: Image.network(
+                            "http://4.bp.blogspot.com/-VlHRlPoXwqU/TtskdJMC4aI/AAAAAAAAAjQ/ooLDREJgZvM/s1600/parrot1.jpg")
+                        .image,
+                    fit: BoxFit.fill),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          const Text(
+            "Full Name",
+            style: TextStyle(fontSize: 25, letterSpacing: 1.5),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Center(
+            child: Text(
+              "Lorem ipsum dolor sit amet,\nconsectetur adipiscingkjdfh elkjhdfit,\n sed do eiusmod tlksaflkalempor .",
+              style: TextStyle(fontSize: 16, wordSpacing: 2),
+              textAlign: TextAlign.center,
             ),
-          ],
-        ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+           onTap: _launchURLBrowserLinkedin,
+            child: Container(
+              color: Color.fromARGB(171, 160, 154, 216),
+              width: 370,
+              height: 50,
+              child: const ListTile(
+                leading: FaIcon(FontAwesomeIcons.linkedin),
+                title: Text("Linkedin"),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: _launchURLBrowserFacebook,
+              
+            child: Container(
+              color: Color.fromARGB(171, 66, 236, 177),
+              width: 370,
+              height: 50,
+              child: const ListTile(
+                leading: FaIcon(FontAwesomeIcons.facebook),
+                title: Text("FaceBook"),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: _launchURLBrowserTwitter,
+            child: Container(
+              color: Color.fromARGB(171, 241, 69, 239),
+              width: 370,
+              height: 50,
+              child: const ListTile(
+                leading: FaIcon(FontAwesomeIcons.twitter),
+                title: Text("Twitter"),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: _launchURLBrowserGitHub,
+            child: Container(
+              color: Color.fromARGB(171, 238, 205, 56),
+              width: 370,
+              height: 50,
+              child: const ListTile(
+                leading: FaIcon(FontAwesomeIcons.github),
+                title: Text("Github"),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            const CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.greenAccent,
+                child: FaIcon(
+                  FontAwesomeIcons.whatsapp,
+                  size: 30,
+                )),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Message(),
+                    ));
+              },
+              child: const CircleAvatar(
+                  radius: 30,
+                  backgroundColor: Color.fromARGB(255, 240, 193, 105),
+                  child: FaIcon(
+                    FontAwesomeIcons.message,
+                    size: 30,
+                  )),
+            ),
+            const CircleAvatar(
+                radius: 30,
+                backgroundColor: Color.fromARGB(255, 213, 240, 105),
+                child: FaIcon(
+                  FontAwesomeIcons.anchor,
+                  size: 30,
+                )),
+          ]),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/message.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Message extends StatelessWidget {
+  const Message({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const Center(
+              child: Padding(
+            padding: EdgeInsets.only(top: 80, bottom: 30),
+            child: Text(
+              "Message",
+              style: TextStyle(fontSize: 25),
+            ),
+          )),
+          Container(
+            height: 125,
+            width: 380,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Color.fromARGB(255, 229, 229, 229),
+                border: Border.all(width: 2, color: Colors.green)),
+            child: const Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                "Name:\nLorem ipsum dolor sit amet, sed do eiusmod tempor  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+                style: TextStyle(fontSize: 16, color: Colors.green),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 125,
+            width: 380,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(255, 229, 229, 229),
+                border: Border.all(width: 2, color: Colors.green)),
+            child: const Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                "Name:\nLorem ipsum dolor sit amet, sed do eiusmod tempor  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+                style: TextStyle(fontSize: 16, color: Colors.green),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 125,
+            width: 380,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color.fromARGB(255, 229, 229, 229),
+                border: Border.all(width: 2, color: Colors.green)),
+            child: const Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                "Name:\nLorem ipsum dolor sit amet, sed do eiusmod tempor  laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit",
+                style: TextStyle(fontSize: 16, color: Colors.green),
+                textAlign: TextAlign.justify,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Divider(thickness: 1.5, color: Colors.grey),
+          const SizedBox(
+            height: 20,
+          ),
+          const Center(
+            child: Text("Send a message" , style: TextStyle(fontSize: 20),),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(onPressed: (){},icon:const Icon(Icons.send)),
+                    hintText: 'Message',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(15),
+                        borderSide: const BorderSide(color: Colors.grey)))),
+          ),
+        ]),
+      ),
+    );
+  }
+}
